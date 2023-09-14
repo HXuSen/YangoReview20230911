d284898648ea2bef8e98b070282835d58b608f35
HuangXuSen <2293046230@qq.com>
2023-09-14 18:19:24 +0800

    redis分布式锁版本3 使用lua脚本保证删除锁时原子性,避免误删

--- src/main/java/com/yango/review/utils/SimpleRedisLock.java
+++ src/main/java/com/yango/review/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.yango.review.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,9 +19,15 @@ import java.util.concurrent.TimeUnit;
 public class SimpleRedisLock implements ILock{
 
     private StringRedisTemplate stringRedisTemplate;
-    public static final String KEY_PREFIX = "lock:";
-    public static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
     private String name;
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate, String name) {
         this.stringRedisTemplate = stringRedisTemplate;
@@ -35,10 +44,16 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unLock() {
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if (threadId.equals(id)){
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
+    //@Override
+    //public void unLock() {
+    //    String threadId = ID_PREFIX + Thread.currentThread().getId();
+    //    String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+    //    if (threadId.equals(id)){
+    //        stringRedisTemplate.delete(KEY_PREFIX + name);
+    //    }
+    //}
 }
