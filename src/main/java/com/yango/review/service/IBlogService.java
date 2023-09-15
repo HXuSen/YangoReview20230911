3c86ec58d20990d4423fa68123da4f351c43644d
HuangXuSen <2293046230@qq.com>
2023-09-15 18:01:46 +0800

    达人探店 1.发布探店笔记 2.查看探店笔记 3.点赞功能 4.点赞排行榜

--- src/main/java/com/yango/review/service/IBlogService.java
+++ src/main/java/com/yango/review/service/IBlogService.java
@@ -1,8 +1,16 @@
 package com.yango.review.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yango.review.dto.Result;
 import com.yango.review.entity.Blog;
 
 public interface IBlogService extends IService<Blog> {
 
+    Result queryBlogById(Long id);
+
+    Result queryHotBlog(Integer current);
+
+    Result likeBlog(Long id);
+
+    Result queryBlogLikes(Long id);
 }
