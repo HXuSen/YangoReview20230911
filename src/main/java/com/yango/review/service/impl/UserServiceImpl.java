4842c5eeeadbeaa990f211af753260a91513cb11
HuangXuSen <2293046230@qq.com>
2023-09-16 17:13:41 +0800

    用户签到 1.每日签到记录 2.统计连续签到

--- src/main/java/com/yango/review/service/impl/UserServiceImpl.java
+++ src/main/java/com/yango/review/service/impl/UserServiceImpl.java
@@ -13,14 +13,19 @@ import com.yango.review.entity.User;
 import com.yango.review.mapper.UserMapper;
 import com.yango.review.service.IUserService;
 import com.yango.review.utils.RegexUtils;
+import com.yango.review.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -91,6 +96,55 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         stringRedisTemplate.expire(LOGIN_USER_KEY + token,LOGIN_USER_TTL,TimeUnit.MINUTES);
         //8返回token
         return Result.ok(token);
+    }
+
+    @Override
+    public Result sign() {
+        //获取用户
+        Long userId = UserHolder.getUser().getId();
+        //获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        //获取今天是本月第几天
+        int dayOfMonth = now.getDayOfMonth();
+        //保存
+        stringRedisTemplate.opsForValue().setBit(key,dayOfMonth - 1,true);
+        return Result.ok();
+    }
+
+    @Override
+    public Result signCount() {
+        //获取用户
+        Long userId = UserHolder.getUser().getId();
+        //获取日期
+        LocalDateTime now = LocalDateTime.now();
+        //拼接key
+        String keySuffix = now.format(DateTimeFormatter.ofPattern(":yyyyMM"));
+        String key = USER_SIGN_KEY + userId + keySuffix;
+        //获取今天是本月第几天
+        int dayOfMonth = now.getDayOfMonth();
+        List<Long> result = stringRedisTemplate.opsForValue().bitField(
+                key, BitFieldSubCommands.create().get(BitFieldSubCommands.BitFieldType.unsigned(dayOfMonth)).valueAt(0)
+        );
+        if (result == null || result.isEmpty()){
+            return Result.ok(0);
+        }
+        Long num = result.get(0);
+        if (num == null || num == 0) {
+            return Result.ok(0);
+        }
+        int dayCount = 0;
+        while (true){
+            if ((num & 1 ) == 0) {
+                break;
+            }else{
+                dayCount++;
+            }
+            num >>>= 1;
+        }
+        return Result.ok(dayCount);
     }
 
     private User createUserWithPhone(String phone) {
